4d986be2a1c4f64f666398e8dc273a7a4149fbdf
philomath213 <bilalphilomath@gmail.com>
2020-08-19 18:48:20 +0100

    update workflow

---
+++ .github/workflows/scripts/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+# install cmake 3.17.1
+curl https://github.com/Kitware/CMake/releases/download/v3.17.1/cmake-3.17.1-Linux-x86_64.tar.gz -L | tar xz -C /opt/
+echo "::add-path::/opt/cmake-3.17.1-Linux-x86_64/bin"
+
+
+# install python dependencies
+python -m pip install --upgrade pip
+pip install setuptools wheel twine auditwheel
